d31932afa50c8c1d07d01ec3e7e8a443d3baad68
Irvin-Tandiarrang-Sumual <irvintandiarrang23@gmail.com>
2025-11-29 21:27:40 +0700

    chore : init next.js FE using HeroUI

---
+++ src/frontend/config/site.ts
@@ -0,0 +1,26 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+  name: "Next.js + HeroUI",
+  description: "Make beautiful websites regardless of your design experience.",
+  navItems: [
+    {
+      label: "Home",
+      href: "/",
+    },
+    {
+      label: "Book Collection",
+      href: "/book-collection",
+    },
+  ],
+  navMenuItems: [
+    {
+      label: "Home",
+      href: "/",
+    },
+    {
+      label: "Book Collection",
+      href: "/book-collection",
+    },
+  ]
+};
