af0571ee110a9c516d7b679975223c95ca213921
Irvin-Tandiarrang-Sumual <irvintandiarrang23@gmail.com>
2025-12-03 21:52:59 +0700

    refactor(main.py): replace global variabel for mapper and model to app state

--- src/backend/main.py
+++ src/backend/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, Depends, Request
 from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
 from pathlib import Path
@@ -20,9 +20,6 @@ MAPPER_PATH = BASE_PATH / "data" / "mapper.json"
 # pca model
 PCA_MODEL_PATH = Path(__file__).parent / "pca_model.pkl"
 
-book_mapper = {}
-pca_model = {}
-
 def load_mapper(path : str):
     if not os.path.exists(path):
         print(f"Mapper file {path} tidak ditemukan")
@@ -34,21 +31,19 @@ def load_mapper(path : str):
 async def lifespan(app:FastAPI):
     print("Starting Server")
 
-    global book_mapper, pca_model
     # load mapper
     print("Load mapper ...")
-    book_mapper = load_mapper(str(MAPPER_PATH))
+    app.state.book_mapper = load_mapper(str(MAPPER_PATH))
     print("Load mapper success ...")
 
     print("Building pca model ...")
     # load pca model
-    pca_model = build_pca_model(
+    app.state.pca_model = build_pca_model(
         dataset_dir= str(COVERS_DIR_PATH),
         model_save_path= str(PCA_MODEL_PATH),
         target_width=200,
         target_height=300,
         k=50,
-        overwrite=True
     )
     print("Building pca model success")
 
@@ -58,6 +53,13 @@ async def lifespan(app:FastAPI):
     print("Shutting Down Server")
 
 app = FastAPI(lifespan=lifespan)
+
+# dependency
+async def get_book_mapper(request: Request):
+    return request.app.state.book_mapper
+
+async def get_pca_model(request: Request):
+    return request.app.state.pca_model
 
 app.add_middleware(
     CORSMiddleware,
@@ -71,24 +73,29 @@ app.add_middleware(
 
 @app.get("/")
 async def read_root():
-    return {"message : Hallo sar!"}
+    return {"message" : "Hallo sar!"}
 
 # (get) all book
 @app.get("/api/books")
-async def read_books():
-    pass
+async def read_books(book_mapper : dict = Depends(get_book_mapper)):
+    return book_mapper
 
 # (get) detail - content
-@app.get("api/books/{book_id}/content")
-async def read_book_detail_content(book_id : int):
+@app.get("/api/books/{book_id}/content")
+async def read_book_detail_content(book_id : str, book_mapper : dict = Depends(get_book_mapper)):
     pass
 
 # (get) detail - recommendation
-@app.get("api/books/{book_id}/recommendation")
-async def read_book_detail_recommendation(book_id : int):
+@app.get("/api/books/{book_id}/recommendation")
+async def read_book_detail_recommendation(book_id : str, book_mapper : dict = Depends(get_book_mapper)):
     pass
 
-@app.post("api/books/")
-# (post) search pake image
+# (get) search pakai judul
+@app.get("/api/books/search")
+async def search_books_by_title(title : str, book_mapper : dict = Depends(get_book_mapper)):
+    pass
 
-# (post) search pake judul
+# (post) search pake image
+@app.post("/api/books/search-by-image")
+async def search_books_by_image(file : UploadFile):
+    pass
