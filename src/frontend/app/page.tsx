52a4bba8d5d6eefc04ecca4736832d682afa9bf9
Irvin-Tandiarrang-Sumual <irvintandiarrang23@gmail.com>
2025-12-05 20:45:14 +0700

    feat: logo lemonnipis

--- src/frontend/app/page.tsx
+++ src/frontend/app/page.tsx
@@ -1,9 +1,19 @@
 import { CustomSearchInput } from "@/components/search-input";
 import { title, subtitle } from "@/components/primitives";
-
+import Image from "next/image";
 export default function Home() {
   return (
-    <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+    <section className="flex flex-col items-center justify-center gap-4 py-4 md:py-5">
+      <div className="mb-2">
+        <Image
+          src="/LemonNipis.png"
+          alt="Lemon Nipis Logo"
+          width={350}
+          height={350}
+          priority
+          className="drop-shadow-lg"
+        />
+      </div>
       <div className="inline-block max-w-xl text-center justify-center">
         <span className={title()}>Welcome to&nbsp;</span>
         <span className={title({ color: "lemon" })}>LemonNipis&nbsp;</span>
